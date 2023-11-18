93512250210a335b1ef1f35ad9f49d5677d03471
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-11-18 12:02:00 -0600

    Completed resume page and worked on styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { AiFillTwitterCircle, AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
     return (
         <footer className=" text-5xl flex justify-center gap-16 py-5 text-gray-600">
                 <a href="https://twitter.com/"><AiFillTwitterCircle /></a>
                 <a href="https://www.linkedin.com/in/chase-ostien-2a1205200/"><AiFillLinkedin /></a>
-                <a href="https://github.com/ChaseOstien"><AiFillGithub /></a>
+                <a href="https://github.com/ChaseOstien"><FontAwesomeIcon icon={faGithub} bounce /></a>
         </footer>
     )
 }
--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -11,16 +11,18 @@ const styles= {
 
 export default function AboutPage () {
     return (
-        <div className="  p-10 flex-col">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium font-burtons">About</h2>
-            <img alt="Profile Picture" src="public/img/ProfilePicSmall.png" className="p-10 bg-gradient-to-b from-teal-600 rounded-full" style={styles.img}></img>
-            <div className="text-center flex-col">
-            <p className="text-xl pt-8 pb-5 leading-8 text-gray-900 justify-center max-w-screen-md" >I am a passionate and dedicated web developer with a keen interest in crafting digital experiences that seamlessly blend creativity and functionality. My journey in the world of coding started with a curiosity to understand the language of the web, and since then, I have immersed myself in the dynamic and ever-evolving landscape of front-end and back-end development.</p>
+        <><div className="flex justify-items-start">
+            <h2 className="text-5xl p-8 text-teal-600 font-medium font-burtons">About</h2>
+        </div><div className="p-10 flex items-center flex-col justify-center">
 
-            <p className="text-xl py-5 leading-8 text-gray-900 text-center max-w-screen-md">Driven by a love for problem-solving and a knack for turning ideas into reality, I thrive on the challenges that come with building innovative solutions. With a solid foundation in HTML, CSS, and JavaScript, coupled with expertise in popular frameworks and libraries, I am committed to delivering clean, efficient, and user-centric web applications.</p>
+                <img alt="Profile Picture" src="/img/ProfilePicSmall.png" className="p-10 bg-gradient-to-b from-teal-600 rounded-full" style={styles.img}></img>
+                <div className="text-center flex-col p-3 md:py-5 md:px-15 shadow-2xl rounded-xl my-10 max-w-2xl bg-slate-300">
+                    <p className="text-xl pt-8 pb-5 leading-8 text-gray-900 justify-center ">I am a passionate and dedicated web developer with a keen interest in crafting digital experiences that seamlessly blend creativity and functionality. My journey in the world of coding started with a curiosity to understand the language of the web, and since then, I have immersed myself in the dynamic and ever-evolving landscape of front-end and back-end development.</p>
 
-            <p className="text-xl py-5 leading-8 text-gray-900 text-center max-w-screen-md">Away from the keyboard, I find inspiration in diverse interests such as graphic design, user experience, and the latest trends in technology. My goal is not just to write code but to create impactful and visually stunning digital experiences that leave a lasting impression. Let's collaborate and bring your ideas to life in the digital realm!</p>
-            </div>
-        </div>
+                    <p className="text-xl py-5 leading-8 text-gray-900 text-center ">Driven by a love for problem-solving and a knack for turning ideas into reality, I thrive on the challenges that come with building innovative solutions. With a solid foundation in HTML, CSS, and JavaScript, coupled with expertise in popular frameworks and libraries, I am committed to delivering clean, efficient, and user-centric web applications.</p>
+
+                    <p className="text-xl py-5 leading-8 text-gray-900 text-center ">Away from the keyboard, I find inspiration in diverse interests such as graphic design, user experience, and the latest trends in technology. My goal is not just to write code but to create impactful and visually stunning digital experiences that leave a lasting impression. Let's collaborate and bring your ideas to life in the digital realm!</p>
+                </div>
+            </div></>
     )
 }
